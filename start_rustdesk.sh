5d2363efbda65f6dc33f2003c7782f85b51a447d
epoweripione <siyuwuxin@gmail.com>
2024-01-04 20:15:16 +0800

    modify output logs

--- start_rustdesk.sh
+++ start_rustdesk.sh
@@ -4,7 +4,7 @@ nohup hbbs -r "${RUSTDESK_RELAY_IP}" -k _ >hbbs.log 2>&1 &
 
 nohup hbbr -k _ >hbbr.log 2>&1 &
 
-echo "[$(date +"%F %T.%6N %:z")] Rustdesk server start success!"
+echo "[$(date +"%F %T.%6N %:z")] Running rustdesk server..."
 echo ""
 
-tail -F hbbs.log
+tail -F hbbs.log hbbr.log
